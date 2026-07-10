04dbff1c0f864b97d06b0ed8e5275b1a82baa53b
choiyongbum <72883647+choiyongbum@users.noreply.github.com>
2026-07-10 22:02:12 +0900

    first commit

---
+++ src/main/java/org/example/commercebackoffice/CommerceBackofficeApplication.java
@@ -0,0 +1,13 @@
+package org.example.commercebackoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceBackofficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommerceBackofficeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/commercebackoffice/CommerceBackofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.commercebackoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceBackofficeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
