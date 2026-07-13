81a1014b2fa924fe9472d6b431900829242c17ae
yun-ji98 <lyj77731@naver.com>
2026-07-13 14:52:38 +0900

    customer delete 완료

--- src/main/java/org/example/commercebackoffice/customer/controller/CustomerController.java
+++ src/main/java/org/example/commercebackoffice/customer/controller/CustomerController.java
@@ -1,4 +1,35 @@
 package org.example.commercebackoffice.customer.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.commercebackoffice.customer.service.CustomerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class CustomerController {
+
+    private final CustomerService customerService;
+
+    // 고객 리스트 조회
+
+
+    // 고객 상세 조회
+
+
+    // 고객 정보 수정
+
+
+    // 고객 상태 변경
+
+
+    // 고객 삭제
+    @DeleteMapping("/customers/{customerId}")
+    public ResponseEntity<Void> delete(@PathVariable Long customerId) {
+        customerService.delete(customerId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
--- src/main/java/org/example/commercebackoffice/customer/service/CustomerService.java
+++ src/main/java/org/example/commercebackoffice/customer/service/CustomerService.java
@@ -1,4 +1,39 @@
 package org.example.commercebackoffice.customer.service;
 
+import lombok.RequiredArgsConstructor;
+import org.example.commercebackoffice.customer.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    // 고객 리스트 조회
+    @Transactional
+
+
+    // 고객 상세 조회
+    @Transactional
+
+
+    // 고객 정보 수정
+    @Transactional
+
+
+    // 고객 상태 변경
+    @Transactional
+
+
+    // 고객 삭제
+    @Transactional
+    public void delete(Long customerId) {
+        boolean existence = customerRepository.existsById(customerId);
+        if (!existence) {
+            throw new IllegalStateException("존재하지 않는 고객입니다.");
+        }
+        customerRepository.deleteById(customerId);
+    }
 }
