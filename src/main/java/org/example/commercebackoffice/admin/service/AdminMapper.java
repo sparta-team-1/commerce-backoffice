0dd53a07acdd360b8f9ba06848866e544120e2f5
test <hello.git.GitHub@lomeafoer@gmail.com>
2026-07-13 20:13:52 +0900

    add: 로그인 기능을 위한 DTO 및 세션 저장을 위한 SessionUser 클래스 추가
    - AdminResponse <-> Admin entity 간의 매핑을 위한 AdminMapper 추가
    
    임시커밋

--- src/main/java/org/example/commercebackoffice/admin/service/AdminMapper.java
+++ src/main/java/org/example/commercebackoffice/admin/service/AdminMapper.java
@@ -1,8 +1,30 @@
 package org.example.commercebackoffice.admin.service;
 
+import org.example.commercebackoffice.admin.controller.auth.SessionUser;
+import org.example.commercebackoffice.admin.controller.admin.dto.response.AdminResponse;
+import org.example.commercebackoffice.admin.domain.Admin;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AdminMapper {
+    public static AdminResponse toAdminResponse(Admin admin) {
+        return new AdminResponse(
+                admin.getId(),
+                admin.getEmail(),
+                admin.getName(),
+                admin.getPhoneNumber(),
+                admin.getRole(),
+                admin.getStatus(),
+                admin.getCreatedAt(),
+                admin.getApprovedAt()
+        );
+    }
 
+    public static SessionUser toSessionUser(Admin admin) {
+        return new SessionUser(
+                admin.getId(),
+                admin.getEmail(),
+                admin.getRole()
+        );
+    }
 }
