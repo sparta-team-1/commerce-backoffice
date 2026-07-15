1971ce2d44530c6c7fc5d7a4f12f0d434b895509
test <hello.git.GitHub@lomeafoer@gmail.com>
2026-07-15 14:05:39 +0900

    add: 관리자 상태 변경 요청 DTO 추가

---
+++ src/main/java/org/example/commercebackoffice/admin/controller/admin/dto/request/AdminChangeStatusRequest.java
@@ -0,0 +1,8 @@
+package org.example.commercebackoffice.admin.controller.admin.dto.request;
+
+import org.example.commercebackoffice.admin.domain.enums.AdminStatus;
+
+public record AdminChangeStatusRequest(
+        AdminStatus status
+) {
+}
