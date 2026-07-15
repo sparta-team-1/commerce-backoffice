b64085af78ed7145aadee48f66c159ea3b4ceabb
강성현 <rdevsh0104@gmail.com>
2026-07-15 11:51:14 +0900

    feat:필수 기능 구현

--- src/main/java/org/example/commercebackoffice/item/domain/enums/ItemStatus.java
+++ src/main/java/org/example/commercebackoffice/item/domain/enums/ItemStatus.java
@@ -1,7 +1,7 @@
 package org.example.commercebackoffice.item.domain.enums;
 
 public enum ItemStatus {
-    ON_SALE, // 판매 중
-    SOLD_OUT, // 품절
-    DISCONTINUED // 판매 중지(단종)
+    ON_SALE,      // 판매 중
+    SOLD_OUT,     // 품절
+    DISCONTINUED  // 판매 중지(단종, Soft Delete)
 }
--- src/main/java/org/example/commercebackoffice/item/repository/ItemRepository.java
+++ src/main/java/org/example/commercebackoffice/item/repository/ItemRepository.java
@@ -14,12 +14,9 @@ import java.util.Optional;
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
-    // 특정 ID로 찾되, 단종(DISCONTINUED) 상태가 아닌 것만 가져오는 메서드
     Optional<Item> findByIdAndStatusNot(Long id, ItemStatus status);
 
-    // [발표 핵심 포인트] 동적 다중 필터 검색 쿼리
-    // 사용자가 키워드, 카테고리, 상태 중 무엇을 검색할지 모릅니다.
-    // 값이 안 들어오면(IS NULL) 해당 조건은 무시하고, 값이 들어왔을 때만(LIKE 또는 =) 조건절이 발동하는 쿼리입니다.
+    // 조건이 들어올 때만 필터링이 걸리는 동적 쿼리
     @Query("SELECT i FROM Item i WHERE i.status != 'DISCONTINUED' " +
             "AND (:keyword IS NULL OR i.name LIKE %:keyword%) " +
             "AND (:category IS NULL OR i.category = :category) " +
