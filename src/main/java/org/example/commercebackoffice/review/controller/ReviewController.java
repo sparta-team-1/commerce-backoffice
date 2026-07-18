6b7b7d836467bdadaa1079f7dafbb16f1586505a
test <hello.git.GitHub@lomeafoer@gmail.com>
2026-07-18 21:41:34 +0900

    fix: 리뷰 ApiResponse 적용 누락 수정
    - 리뷰 api에 ApiResponse 적용

--- src/main/java/org/example/commercebackoffice/review/controller/ReviewController.java
+++ src/main/java/org/example/commercebackoffice/review/controller/ReviewController.java
@@ -2,6 +2,8 @@ package org.example.commercebackoffice.review.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.example.commercebackoffice.common.dto.ApiResponse;
+import org.example.commercebackoffice.common.message.SuccessCode;
 import org.example.commercebackoffice.order.domain.dto.PageResponseDto;
 import org.example.commercebackoffice.review.domain.dto.*;
 import org.example.commercebackoffice.review.service.ReviewService;
@@ -17,34 +19,46 @@ public class ReviewController {
     private final ReviewService reviewService;
 
     @GetMapping //Get/api/reviews 요청을 이 메서드가 처리한다
-    public ResponseEntity<PageResponseDto<ReviewListResponseDto>> getReviews(@ModelAttribute ReviewSearchCondition condition) {
-        return ResponseEntity.ok(reviewService.getReviews(condition));
+    public ResponseEntity<ApiResponse<PageResponseDto<ReviewListResponseDto>>> getReviews(@ModelAttribute ReviewSearchCondition condition) {
+        return ResponseEntity.ok(ApiResponse.of(
+                SuccessCode.REVIEW_LIST_SELECT_SUCCESS,
+                reviewService.getReviews((condition))
+        ));
     }
 
     @PostMapping
-    public ResponseEntity<ReviewResponseDto> createReview(@Valid @RequestBody ReviewCreateRequestDto requestDto) {
+    public ResponseEntity<ApiResponse<ReviewResponseDto>> createReview(@Valid @RequestBody ReviewCreateRequestDto requestDto) {
         ReviewResponseDto response = reviewService.createReview(requestDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.of(
+                SuccessCode.REVIEW_CREATE_SUCCESS,
+                response
+        ));
     }
 
     //리뷰 상세 조회
     @GetMapping("/{reviewId}")
-    public ResponseEntity<ReviewDetailResponseDto> getReviewDetail(
+    public ResponseEntity<ApiResponse<ReviewDetailResponseDto>> getReviewDetail(
             @PathVariable Long reviewId
     ) {
-      ReviewDetailResponseDto response   = reviewService.getReviewDetail(reviewId);
+      ReviewDetailResponseDto response  = reviewService.getReviewDetail(reviewId);
 
-      return ResponseEntity.ok(response);
+      return ResponseEntity.ok(ApiResponse.of(
+              SuccessCode.REVIEW_DETAIL_SELECT_SUCCESS,
+              response
+      ));
     }
     //리뷰 삭제
     @DeleteMapping("/{reviewId}")
-    public ResponseEntity<Void> deleteReview (
+    public ResponseEntity<ApiResponse<Void>> deleteReview (
             @PathVariable Long reviewId
     ) {
         //삭제할 리뷰 ID를 서비스에 전달
         reviewService.deleteReview(reviewId);
 
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(ApiResponse.of(
+                SuccessCode.REVIEW_DELETE_SUCCESS,
+                null
+        ));
         //삭제 후 클라이언트에게 돌려줄 데이터가 없기 때문에 응답 본문을 비워둔다.
     }
 }
